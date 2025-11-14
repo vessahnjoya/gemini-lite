cf41751a4d80ffa9d93d52f8c48b7a299a62e95a
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2025-11-14 15:02:11 +0100

    possible fix again, creating a new runner to take parameters for redirecting

--- gemini_lite/engine/ClientEngine.java
+++ gemini_lite/engine/ClientEngine.java
@@ -1,9 +1,8 @@
 package engine;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
-
-import gemini_lite.Client;
 import protocol.*;
 
 /**
@@ -15,7 +14,6 @@ public class ClientEngine implements Engine {
     private final URI uri;
     private final int MAX_REDIRECTS = 5;
     private final int DEFAULT_PORT = 1958;
-    private int global_counter = 0;
 
     /**
      * Constructor to initialize URI
@@ -62,9 +60,25 @@ public class ClientEngine implements Engine {
         return uri.getHost();
     }
 
+    private void reader(InputStream i) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = i.read(buffer)) != -1) {
+            System.out.write(buffer, 0, read);
+        }
+        System.out.print("\r\n");
+    }
+
     @Override
     public void run() throws IOException {
+        runWithRedirect(uri, 0);
+    }
 
+    private void runWithRedirect(URI current, int count) throws IOException {
+        if (count > MAX_REDIRECTS) {
+            System.err.println("Maximum redirects amount reached");
+            System.exit(1);
+        }
         try (var socket = new Socket(getHost(), getPort())) {
             final var i = socket.getInputStream();
             final var o = socket.getOutputStream();
@@ -77,20 +91,11 @@ public class ClientEngine implements Engine {
             System.out.print(reply.getStatusCode() + " " + reply.getMeta() + "\r\n");
 
             if (reply.getStatusCode() == 20) {
-                byte[] buffer = new byte[1024];
-                int read;
-                while ((read = i.read(buffer)) != -1) {
-                    System.out.write(buffer, 0, read);
-                }
-                System.out.print("\r\n");
+                reader(i);
                 System.out.flush();
                 System.exit(0);
             } else if (reply.getStatusCode() >= 30 && reply.getStatusCode() < 40) {
-                if (global_counter > MAX_REDIRECTS) {
-                    System.err.println("Too many redirects");
-                    System.exit(1);
-                }
-                HandleRedirect(uri, global_counter, reply.getMeta().trim());
+                HandleRedirect(uri, count, reply.getMeta().trim());
             } else {
                 System.out.flush();
                 System.exit(1);
@@ -118,7 +123,6 @@ public class ClientEngine implements Engine {
             System.err.println("Non gemini-lite URL: " + target);
         }
 
-        global_counter++;
-         new ClientEngine(target).run();
+        runWithRedirect(target, count + 1);
     }
 }
