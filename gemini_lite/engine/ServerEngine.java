9f282172bf76f4f2c9a2d58857a89bfdc004fb66
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2025-11-19 19:50:25 +0100

    server egine now implimented

--- gemini_lite/engine/ServerEngine.java
+++ gemini_lite/engine/ServerEngine.java
@@ -1,6 +1,8 @@
 package engine;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.*;
 
 import handler.ResourceHandler;
@@ -11,7 +13,6 @@ public class ServerEngine implements Engine {
     private final int port;
     private final ResourceHandler resourceHandler;
     private final int DEFAULT_PORT = 1958;
-
 
     /**
      * Constructor to initialize URI
@@ -39,23 +40,66 @@ public class ServerEngine implements Engine {
     @Override
     public void run() throws IOException {
 
-         try (final var server = new ServerSocket(getPort())) {
-            System.err.println("Listening on port " + port);
+        try (final var server = new ServerSocket(getPort())) {
+            System.err.println("Listening on port " + getPort());
             while (true) {
-                final var socket = server.accept();
-                handleConnection(socket);
+                Socket socket = null;
+                try {
+                    socket = server.accept();
+                    handleConnection(socket);
+                } catch (Exception e) {
+                    System.err.println("Error handling connection: " + e.getMessage());
+                    if (socket != null || !socket.isClosed()) {
+                        sendErrorReply(socket, new Reply(50, "Server Error"));
+                    }
+                }
             }
         }
     }
 
-
     public void handleConnection(Socket socket) throws IOException {
+        Request request = null;
+        Reply reply = null;
+        var i = socket.getInputStream();
+        var o = socket.getOutputStream();
         try {
-            final var i = socket.getInputStream();
-            final var o = socket.getOutputStream();
 
-            var request = Request.parse(i);
-            o.flush();
+            request = Request.parse(i);
+            reply = resourceHandler.handle(request);
+
+        } catch (ProtocolSyntaxException e) {
+            reply = new Reply(59, "Bad Request");
+            System.err.println("Protocol error: " + e.getMessage());
+        } catch (IOException e) {
+            reply = new Reply(50, "Server error");
+            System.err.println("Unexpected Error: " + e.getMessage());
+        }
+
+        if (reply != null) {
+            reply.format(o);
+        }
+
+        if (reply.getStatusCode() == 20 && request != null) {
+            try (var in = resourceHandler.getData(request)) {
+                transferData(in, o);
+            }
+        }
+    }
+
+    private void transferData(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+
+        out.flush();
+    }
+
+    private void sendErrorReply(Socket socket, Reply reply) throws IOException {
+        try (var out = socket.getOutputStream()) {
+            reply.format(out);
         } finally {
             socket.close();
         }
