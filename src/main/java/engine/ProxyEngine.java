45ac3388972a416b44bce00ed9ab643b87e1a497
Louis Nathan Vessah Njoya Tchuente <vessahnjoyatchuentelouisnathan@gmail.com>
2025-12-03 19:04:44 +0100

    timeout fix + refactoring of proxy engine

--- src/main/java/engine/ProxyEngine.java
+++ src/main/java/engine/ProxyEngine.java
@@ -5,6 +5,7 @@ import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 
 import protocol.Reply;
@@ -79,42 +80,8 @@ public class ProxyEngine implements Engine {
                     }
 
                     if (reply.getStatusCode() == 44) {
-                        int seconds;
-                        try {
-                            seconds = Integer.parseInt(reply.getMeta().trim());
-                        } catch (NumberFormatException e) {
-                            reply.format(clientOut);
-                            replySent = true;
-                            clientOut.flush();
-                            return;
-                        }
-
-                        try {
-                            Thread.sleep(seconds * 1000L);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            sendProxyError(clientOut, "Proxy error: Interrupted slow down");
-                            clientOut.flush();
-                            return;
-                        }
-
-                        try (var retrySocket = new Socket()) {
-                            retrySocket.connect(new InetSocketAddress(host, port));
-
-                            try (var retryInput = new BufferedInputStream(retrySocket.getInputStream());
-                                    var retryOuput = new BufferedOutputStream(retrySocket.getOutputStream())) {
-                                request.format(retryOuput);
-                                var retryReply = Reply.parse(retryInput);
-                                retryReply.format(clientOut);
-
-                                if (retryReply.getStatusCode() >= 20 && retryReply.getStatusCode() < 30) {
-                                    retryInput.transferTo(clientOut);
-                                }
-                                clientOut.flush();
-                                return;
-                            }
-
-                        }
+                        slowDownHandler(host, port, request, reply, clientOut);
+                        return;
                     } else if (reply.getStatusCode() < 10 || reply.getStatusCode() > 59) {
                         var rep = new Reply(PROXY_ERROR_CODE, reply.getMeta());
                         rep.format(clientOut);
@@ -122,50 +89,22 @@ public class ProxyEngine implements Engine {
                         clientOut.flush();
                         return;
                     } else if (reply.getStatusCode() >= 30 && reply.getStatusCode() < 40) {
-                        int count = 0;
-
-                        while (reply.getStatusCode() >= 30 && reply.getStatusCode() < 40 && count < MAX_REDIRECTS) {
-                            count++;
-                            String redirectString;
-                            if (reply.getMeta().startsWith(URI_SCHEME)) {
-                                redirectString = reply.getMeta();
-                            } else {
-                                redirectString = request.getUri().resolve(reply.getMeta()).toString();
-                            }
-                            var redirectUri = new URI(redirectString);
-                            try (var redirectSocket = new Socket()) {
-                                redirectSocket.connect(new InetSocketAddress(redirectUri.getHost(), DEFAULT_PORT));
-
-                                try (var rin = new BufferedInputStream(redirectSocket.getInputStream());
-                                        var rout = new BufferedOutputStream(redirectSocket.getOutputStream())) {
-
-                                    var redirectRequest = new Request(redirectString);
-                                    redirectRequest.format(rout);
-
-                                    reply = Reply.parse(rin);
-
-                                    reply.format(clientOut);
-                                    replySent = true;
-
-                                    if (reply.getStatusCode() >= 20 && reply.getStatusCode() <= 29) {
-                                        rin.transferTo(clientOut);
-                                        clientOut.flush();
-                                        return;
-                                    }
-
-                                }
-
-                            }
-
-                        }
-                        if (reply.getStatusCode() >= 30 && reply.getStatusCode() < 40) {
+                        var handled = redirectHandler(request, reply, clientOut);
+                        if (!handled) {
                             sendProxyError(clientOut, "Proxy error: too many redirects");
-                            replySent = true;
-                            clientOut.flush();
-                            return;
                         }
+
+                        clientOut.flush();
+                        return;
 
                     }
+
+                    reply.format(clientOut);
+                    if (reply.getStatusCode() >= 20 && reply.getStatusCode() < 30) {
+                        in.transferTo(clientOut);
+                    }
+                    clientOut.flush();
+                    return;
 
                 } catch (Exception e) {
                     if (!replySent) {
@@ -178,6 +117,92 @@ public class ProxyEngine implements Engine {
             }
             clientSocket.close();
         }
+    }
+
+    private boolean redirectHandler(Request originalRequest, Reply initialReply, BufferedOutputStream clientOut)
+            throws URISyntaxException, IOException {
+        var reply = initialReply;
+        int count = 0;
+
+        while (reply.getStatusCode() >= 30 && reply.getStatusCode() < 40 && count < MAX_REDIRECTS) {
+            count++;
+            String redirectString;
+            if (reply.getMeta().startsWith(URI_SCHEME)) {
+                redirectString = reply.getMeta();
+            } else {
+                redirectString = originalRequest.getUri().resolve(reply.getMeta()).toString();
+            }
+            URI redirectUri;
+            try {
+                redirectUri = new URI(redirectString);
+
+            } catch (Exception e) {
+                sendProxyError(clientOut, "Proxy error: invalid request URI");
+                clientOut.flush();
+                return true;
+            }
+
+            try (var redirectSocket = new Socket()) {
+                redirectSocket.connect(new InetSocketAddress(redirectUri.getHost(), DEFAULT_PORT));
+
+                try (var rin = new BufferedInputStream(redirectSocket.getInputStream());
+                        var rout = new BufferedOutputStream(redirectSocket.getOutputStream())) {
+
+                    var redirectRequest = new Request(redirectString);
+                    redirectRequest.format(rout);
+
+                    reply = Reply.parse(rin);
+                    reply.format(clientOut);
+                    if (reply.getStatusCode() >= 20 && reply.getStatusCode() < 30) {
+                        rin.transferTo(clientOut);
+                        return true;
+                    }
+                }
+
+            }
+
+        }
+
+        return reply.getStatusCode() < 30 || reply.getStatusCode() >= 40;
+    }
+
+    private void slowDownHandler(String host, int port, Request request, Reply slowDownReply,
+            BufferedOutputStream clientOut) throws IOException {
+        int seconds;
+        try {
+            seconds = Integer.parseInt(slowDownReply.getMeta().trim());
+        } catch (NumberFormatException e) {
+            slowDownReply.format(clientOut);
+            clientOut.flush();
+            return;
+        }
+
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            sendProxyError(clientOut, "Proxy error: Interrupted slow down");
+            clientOut.flush();
+            return;
+        }
+
+        try (var retrySocket = new Socket()) {
+            retrySocket.connect(new InetSocketAddress(host, port));
+
+            try (var retryInput = new BufferedInputStream(retrySocket.getInputStream());
+                    var retryOuput = new BufferedOutputStream(retrySocket.getOutputStream())) {
+                request.format(retryOuput);
+                var retryReply = Reply.parse(retryInput);
+                retryReply.format(clientOut);
+
+                if (retryReply.getStatusCode() >= 20 && retryReply.getStatusCode() < 30) {
+                    retryInput.transferTo(clientOut);
+                }
+                clientOut.flush();
+            }
+
+        }
+
     }
 
     private void sendProxyError(BufferedOutputStream out, String meta) throws IOException {
